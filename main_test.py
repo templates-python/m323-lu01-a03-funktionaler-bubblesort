f42482f5dea1390aa5256fa25e6376cecbb64559
graphics80 <kevin.maurizi@bzz.ch>
2024-08-21 10:04:07 +0200

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -3,11 +3,11 @@ import main
 def test_sort_presorted():
     # Testfall 1: Testen einer bereits sortierten Liste
     numbers = [11, 22, 34, 45, 64, 90]
-    main.sort(numbers)
-    assert numbers == [11, 22, 34, 45, 64, 90], "Testfall 1 fehlgeschlagen"
+    arr = main.sort(numbers)
+    assert arr == [11, 22, 34, 45, 64, 90], "Testfall 1 fehlgeschlagen"
 
 def test_sort_inverse():
     # Testfall 2: Testen einer umgekehrt sortierten Liste
     numbers = [90, 64, 45, 34, 22, 11]
-    main.sort(numbers)
-    assert numbers == [11, 22, 34, 45, 64, 90], "Testfall 2 fehlgeschlagen"
+    arr = main.sort(numbers)
+    assert arr == [11, 22, 34, 45, 64, 90], "Testfall 2 fehlgeschlagen"
